17dc86d6cac82a99897d279675aa3685336420a0
Joe Becher <joe@codecov.io>
2020-10-06 10:05:10 -0400

    use echo

--- test_elif.sh
+++ test_elif.sh
@@ -9,27 +9,27 @@ then
   mc=0
   if [ -n "$pr" ] && [ "$pr" != false ];
   then
-    say "if 1, line 12"
-    say "is a pr"
+    echo "if 1, line 12"
+    echo "is a pr"
     mc=1
   fi
   if [[ "$mc" =~ ^[a-z0-9]{40}[[:space:]][a-z0-9]{40}$ ]];
   then
-    say "if 2, line 18"
-    say "    Fixing merge commit SHA"
+    echo "if 2, line 18"
+    echo "    Fixing merge commit SHA"
     commit=1
   elif [ "$GIT_COMMIT" != "" ];
   then
-    say "first elif, line 23"
+    echo "first elif, line 23"
     commit="$GIT_COMMIT"
   elif [ "$commit" = "" ];
   then
-    say "second elif, line 27"
+    echo "second elif, line 27"
     commit=2
   fi
 else
-  say "else"
+  echo "else"
   commit="$commit_o"
 fi
 
-say "end: GIT_COMMIT = $GIT_COMMIT, commit_o = $commit_o, pr = $pr" 
+echo "end: GIT_COMMIT = $GIT_COMMIT, commit_o = $commit_o, pr = $pr" 
